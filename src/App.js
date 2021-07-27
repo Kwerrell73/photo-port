4e0a8b828719c0179a894a53d32d85660844b078
Kellie Werrell <kelliewerrell@gmail.com>
2021-07-27 10:05:32 -0700

    added code from module

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
+
   return (
     <div>
-    <Nav></Nav>
-    <main>
-      <About></About>
-    </main>
-  </div>
+      <Nav />
+      <main>
+          <About />        
+      </main>
+    </div>
   );
 }
 
